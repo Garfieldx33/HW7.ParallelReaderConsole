7879c4af1ec2b7617cd79f5ff87e73aa5f9e782c
DESKTOP-TJ96IS1\LAS <aleonchenko@intas-company.com>
2024-02-08 15:40:59 +0500

    InitCommit

---
+++ HW7.ParallelReaderConsole/Program.cs
@@ -0,0 +1,42 @@
+﻿// See https://aka.ms/new-console-template for more information
+using HW7.ParallelReaderConsole;
+using System.IO;
+
+Console.WriteLine("Введите путь к папке, из которой лежать файлы для чтения");
+//string path = Console.ReadLine();
+string path = @"C:\txts\";
+if (path != string.Empty)
+{
+    if (Directory.Exists(path))
+    {
+        List<string> files = SearchTxtFilesInFolder(path);
+
+        Console.WriteLine("Запускаем синхронное чтение и подсчет пробелов");
+        double syncTime= WhiteSpaceCounter.GetTimeOfCounting(files);
+        Console.WriteLine($"Синхронное выполнение операций завершено за {syncTime} секунд");
+
+        Console.WriteLine("Запускаем асинхронное чтение и подсчет пробелов");
+        double asyncTime = WhiteSpaceCounter.GetTimeOfCountingAsync(files).Result;
+        Console.WriteLine($"Асинхронное выполнение операций завершено за {asyncTime} секунд");
+
+        Console.WriteLine("Запускаем параллельное чтение и подсчет пробелов");
+        double parTime = WhiteSpaceCounter.GetTimeOfCountingParallel(files).Result;
+        Console.WriteLine($"Параллельное выполнение операций завершено за {parTime} секунд");
+    }
+    else
+    {
+        Console.WriteLine("Папка по указанному пути не найдена");
+    }
+}
+else
+{
+    Console.WriteLine("Перезапустите приложение и введите правильный путь к папке с файлами");
+}
+Console.WriteLine("Нажмите на любую клавишу для выхода");
+Console.ReadKey();
+
+
+static List<string> SearchTxtFilesInFolder(string basePath)
+{
+    return Directory.GetFiles(basePath, "*.txt", SearchOption.TopDirectoryOnly).ToList();
+}
---
+++ HW7.ParallelReaderConsole/WhiteSpaceCounter.cs
@@ -0,0 +1,120 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics;
+using System.Security.Cryptography.X509Certificates;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HW7.ParallelReaderConsole
+{
+    public static class WhiteSpaceCounter
+    {
+        private static int CountSpacesInString(string inputString)
+        {
+            int count = 0;
+            for (int i = 0; i < inputString.Length; i++)
+            {
+                if (inputString[i] == ' ')
+                {
+                    count++;
+                }
+            }
+            return count;
+        }
+        private static string ReadStringsFromFile(string filePath)
+        {
+            string outputString = string.Empty;
+            if (File.Exists(filePath))
+            {
+                using (StreamReader streamReader = new StreamReader(filePath, Encoding.UTF8))
+                {
+                    outputString = streamReader.ReadToEnd();
+                }
+            }
+            return outputString;
+        }
+
+        private static Task<int> CountSpacesInStringAsync(string inputString)
+        {
+            return Task.Run(() => 
+            {
+                int count = 0;
+                for (int i = 0; i < inputString.Length; i++)
+                {
+                    if (inputString[i] == ' ')
+                    {
+                        count++;
+                    }
+                }
+                return count;
+            });
+        }
+        private static async Task<string> ReadStringsFromFileAsync(string filePath)
+        {
+            string outputString = string.Empty;
+            using (StreamReader streamReader = new StreamReader(filePath, Encoding.UTF8))
+            {
+                outputString = await streamReader.ReadToEndAsync();
+            }
+            return outputString;
+        }
+
+
+
+        public static double GetTimeOfCounting(List<string> filesPath)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+
+            foreach (string file in filesPath)
+            {
+                string fileStrings = ReadStringsFromFile(file);
+                int spacesCount = CountSpacesInString(fileStrings);
+                Console.WriteLine($"В файле {file} содержится {spacesCount} пробелов");
+            }
+            timer.Stop();
+            return timer.Elapsed.TotalSeconds;
+        }
+        
+        public static async Task<double> GetTimeOfCountingAsync(List<string> filesPath)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+
+            foreach (string file in filesPath)
+            {
+                string fileStrings = ReadStringsFromFile(file);
+                int spacesCount = await CountSpacesInStringAsync(fileStrings);
+                Console.WriteLine($"В файле {file} содержится {spacesCount} пробелов");
+            }
+            timer.Stop();
+            return timer.Elapsed.TotalSeconds;
+        }
+
+        public static async Task<double> GetTimeOfCountingParallel(List<string> filesPath)
+        {
+            var timer = new Stopwatch();
+            timer.Start();
+            List<Task> readTasks = new List<Task>();
+
+            foreach (string file in filesPath)
+            {
+                Task t = ReadFromFileAndCountSpaces(file);
+                readTasks.Add(t);
+            }
+            await Task.WhenAll(readTasks);
+
+            timer.Stop();
+            return timer.Elapsed.TotalSeconds;
+        }
+
+        private static Task ReadFromFileAndCountSpaces(string filePath)
+        {
+            return Task.Factory.StartNew(() =>
+            {
+                string fileStrings = ReadStringsFromFile(filePath);
+                int spacesCount = CountSpacesInString(fileStrings);
+                Console.WriteLine($"В файле {filePath} содержится {spacesCount} пробелов");
+            });
+        }
+    }
+}
